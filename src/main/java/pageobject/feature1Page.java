7923e1cd0ce0c9b6caa2287d55fb350f4fde78fe
Brutalation <brutalation@gmail.com>
2020-04-26 06:42:33 -0400

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature1Page.java
+++ src/main/java/pageobject/feature1Page.java
@@ -25,6 +25,8 @@ public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]"
 public static By searchBox = By.xpath("//input[@title='Search']");
 public static By button60 = By.xpath("(//input[contains(@class,'u-btn u-btn--primary u-btn--block qa-cookie-button')])[1]");
 
+public static By textbox70 = By.xpath("(//input[contains(@class,'c-form-group__text-input c-form-group__text-input--pronounced c-form-group__text-input--username username qa-username-input')])[1]");
+
 //<XPATHS>
 
 /*Page Constructor*/
@@ -54,6 +56,28 @@ public WebElement clickButton(String objectName) throws Exception {
         }
         return element;
     }
+
+public void writeText(String text, String objectName) throws Exception {
+        //Write Text
+        String actualText = null;
+        Field field = null;
+        By by= null;
+        WebElement element=null;
+        try {
+            field = this.getClass().getField(objectName);
+            by = (By) field.get(this);
+            element = driver.findElement(by);
+            element.clear();
+            element.sendKeys(text);
+            actualText = driver.findElement(by).getAttribute("value");
+            element=null;
+        }catch (Exception e){
+            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
+        }
+       //Assert write text
+        assertEquals(text, actualText);
+    }
+
 
 //<METHOD>
 }
