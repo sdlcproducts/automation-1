12f9b586acabc50a0b87bd73b96070eaae457634
brutalation <brutalation@gmail.com>
2020-04-28 20:27:32 -0400

    Updating POM, Runner and Reports in order to execute tests from command line

--- src/main/java/pageobject/feature1Page.java
+++ src/main/java/pageobject/feature1Page.java
@@ -23,9 +23,9 @@ public static Properties properties;
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
 public static By searchBox = By.xpath("//input[@title='Search']");
-public static By button60 = By.xpath("(//input[contains(@class,'u-btn u-btn--primary u-btn--block qa-continue-button')])[1]");
+public static By button70 = By.xpath("(//input[contains(@class,'u-btn u-btn--primary u-btn--block qa-continue-button')])[1]");
 
-public static By textbox70 = By.xpath("(//input[contains(@class,'c-form-group__text-input c-form-group__text-input--pronounced c-form-group__text-input--username username qa-username-input')])[1]");
+public static By textbox60 = By.xpath("(//input[contains(@class,'c-form-group__text-input c-form-group__text-input--pronounced c-form-group__text-input--username username qa-username-input')])[1]");
 
 //<XPATHS>
 
--- src/test/features/feature1Test.feature
+++
@@ -1,7 +0,0 @@
-@Wow
-Feature: This setups the automation project
-  Background:
- 	Given user opens "Allegis" portal on "TEST" environment
- Scenario: Type in page
- 	  Given user clicks "button60" button
-      Then user types into "textbox70" textbox
