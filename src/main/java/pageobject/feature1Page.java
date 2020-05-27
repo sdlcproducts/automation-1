4350e00ee62276043bd8becc96206efed41cec38
Brutalation <brutalation@gmail.com>
2020-05-26 22:06:07 -0400

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature1Page.java
+++ src/main/java/pageobject/feature1Page.java
@@ -25,9 +25,9 @@ public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]"
 public static By searchBox = By.xpath("//input[@title='Search']");
 public static By link60 = By.xpath("(//a[contains(.,'Companies')])[2]");
 
-public static By link70 = By.xpath("(//div[@data-id='paypal'])[2]");
+public static By link70 = By.xpath("(//div[.='PayPal'])[2]");
 
-public static By link80 = By.xpath("(//div[@data-id='paypal'])[2]");
+public static By link80 = By.xpath("(//div[.='PayPal'])[2]");
 
 public static By link100 = By.xpath("(//li[@class='-pointer'])[2]");
 
