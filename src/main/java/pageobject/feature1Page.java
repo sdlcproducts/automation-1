210d0ddbf1febb46ad638d61b69910489a7a64c8
Brutalation <brutalation@gmail.com>
2020-05-29 02:39:19 -0400

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature1Page.java
+++ src/main/java/pageobject/feature1Page.java
@@ -23,15 +23,7 @@ public static Properties properties;
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
 public static By searchBox = By.xpath("//input[@title='Search']");
-public static By link60 = By.xpath("(//a[contains(.,'Companies')])[2]");
-
-public static By link70 = By.xpath("(//div[.='PayPal'])[2]");
-
-public static By link80 = By.xpath("(//div[.='PayPal'])[2]");
-
-public static By button100 = By.xpath("(//li[@class='-pointer'])[2]");
-
-public static By text111 = By.xpath("//input");
+public static By link60 = By.xpath("//a[.='Portfolio']");
 
 //<XPATHS>
 
@@ -45,37 +37,6 @@ public feature1Page(WebDriver driver){
 this.driver= driver;
 myDriver = new AADriver(this.driver);
 }
-
-public WebElement clickButton(String objectName) throws Exception {
-        //Write Text
-       Field field = null;
-        By by= null;
-        WebElement element=null;
-        try {
-            field = this.getClass().getField(objectName);
-            by = (By) field.get(this);
-            element = driver.findElement(by);
-            element.click();
-        }catch (Exception e){
-            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
-        }
-        return element;
-    }
-
- public boolean elementExists(String objectName) throws Exception {
-    //Element exists
-    Field field = null;
-    By by= null;
-    boolean status;
-    try {
-      field = this.getClass().getField(objectName);
-      by = (By) field.get(this);
-      status = myDriver.verifyObjectPresent(by);
-    }catch (Exception e){
-      throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
-    }
-    return status;
-  }
 
 public WebElement clickLink(String objectName) throws Exception {
         //Click Link
@@ -92,28 +53,6 @@ public WebElement clickLink(String objectName) throws Exception {
         }
         return element;
     }
-
-public void writeText(String text, String objectName) throws Exception {
-        //Write Text
-        String actualText = null;
-        Field field = null;
-        By by= null;
-        WebElement element=null;
-        try {
-            field = this.getClass().getField(objectName);
-            by = (By) field.get(this);
-            element = driver.findElement(by);
-            element.clear();
-            element.sendKeys(text);
-            actualText = driver.findElement(by).getAttribute("value");
-            element=null;
-        }catch (Exception e){
-            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
-        }
-       //Assert write text
-        assertEquals(text, actualText);
-    }
-
 
 //<METHOD>
 }
