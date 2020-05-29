ac54521ee06097a3d7ca8facb385b92633d5e6a5
Brutalation <brutalation@gmail.com>
2020-05-29 05:01:01 -0400

    Commtting File: StepDefinitionFile

--- src/main/java/glue/feature1StepDef.java
+++ src/main/java/glue/feature1StepDef.java
@@ -68,41 +68,30 @@ driver.get(param11);
 //<CODE>
 }
 
- @When("^user clicks \"([^\"]*)\" Link$")
+ @Then("^user sees \"([^\"]*)\" element$")
 public void method2(String param21) throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//link1
-feature1Page feature1Page = new feature1Page(driver);
-feature1Page.clickLink(param21);
-//<CODE>
-}
-
- @Then("^user scrolls to \"([^\"]*)\" element$")
-public void method3(String param31) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //exists1
 feature1Page feature1Page = new feature1Page(driver);
-feature1Page.elementExists(param31);
-
+feature1Page.elementExists(param21);
 //<CODE>
 }
 
  @Then("^user clicks \"([^\"]*)\" link$")
-public void method4(String param41) throws Throwable {
+public void method3(String param31) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //link1
 feature1Page feature1Page = new feature1Page(driver);
-feature1Page.clickLink(param41);
-
+feature1Page.clickLink(param31);
 //<CODE>
 }
 
- @Then("^user sees \"([^\"]*)\" element$")
-public void method6(String param61) throws Throwable {
+ @Then("^user scrolls to \"([^\"]*)\" link$")
+public void method4(String param41) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
-//exists1
+//null
 feature1Page feature1Page = new feature1Page(driver);
-feature1Page.elementExists(param61);
+feature1Page.clickLink(param41);
 //<CODE>
 }
 
