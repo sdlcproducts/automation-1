bde5572dcc31ca97ad0dc9e86b2e72a4ff5d4585
Brutalation <brutalation@gmail.com>
2020-06-02 04:27:46 -0400

    Commtting File: StepDefinitionFile

--- src/main/java/glue/feature1StepDef.java
+++ src/main/java/glue/feature1StepDef.java
@@ -25,6 +25,9 @@ import org.junit.*;
 
 import pageobject.feature1Page;
 
+import org.apache.commons.lang.RandomStringUtils;
+import pageobject.feature1Page;
+
 //<IMPORTS>
 
 public class feature1StepDef {
@@ -65,7 +68,7 @@ Thread.sleep(10000);
 }
 
 
-@Given("^user navigates to Kleiner Pekins Homepage \"([^\"]*)\"$")
+@Given("^user navigates to \"([^\"]*)\"$")
 public void method1(String param11) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //navigation2
@@ -73,55 +76,40 @@ driver.get(param11);
 //<CODE>
 }
 
- @When("^sees the \"([^\"]*)\" element$")
+ @When("^user clicks \"([^\"]*)\" button$")
 public void method2(String param21) throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//exists1
-feature1Page feature1Page = new feature1Page(driver);
-feature1Page.elementExists(param21);
-
-
-//<CODE>
-}
-
- @Then("^user clicks the \"([^\"]*)\" button$")
-public void method3(String param31) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //button1
 feature1Page feature1Page = new feature1Page(driver);
-feature1Page.clickButton(param31);
-
-
-//<CODE>
-}
-
- @Then("^sees \"([^\"]*)\" element$")
-public void method4(String param41) throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//exists1
-feature1Page feature1Page = new feature1Page(driver);
-feature1Page.elementExists(param41);
-
-//<CODE>
-}
-
- @And("^clicks \"([^\"]*)\" link$")
-public void method5(String param51) throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//link1
-feature1Page feature1Page = new feature1Page(driver);
-feature1Page.clickLink(param51);
+feature1Page.clickButton(param21);
 
 //<CODE>
 }
 
  @Then("^user sees \"([^\"]*)\" element$")
-public void method6(String param61) throws Throwable {
+public void method3(String param31) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //exists1
 feature1Page feature1Page = new feature1Page(driver);
-feature1Page.elementExists(param61);
+feature1Page.elementExists(param31);
+//<CODE>
+}
 
+ @Then("^user clicks \"([^\"]*)\" link$")
+public void method4(String param41) throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//link1
+feature1Page feature1Page = new feature1Page(driver);
+feature1Page.clickLink(param41);
+//<CODE>
+}
+
+ @Then("^user types in \"([^\"]*)\" in \"([^\"]*)\" textbox$")
+public void method6(String param61, String param62) throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//text2
+feature1Page feature1Page = new feature1Page(driver);
+feature1Page.writeText(param61, param62);
 //<CODE>
 }
 
