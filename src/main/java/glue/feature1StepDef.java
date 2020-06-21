12faeca4dd8a380afbfd5eb4dba67035ed6196a0
Brutalation <brutalation@gmail.com>
2020-06-21 14:42:50 -0400

    Commtting File: StepDefinitionFile

--- src/main/java/glue/feature1StepDef.java
+++ src/main/java/glue/feature1StepDef.java
@@ -28,70 +28,70 @@ import pageobject.feature1Page;
 //<IMPORTS>
 
 public class feature1StepDef {
-  private WebDriverFactory contextSteps;
-  private WebDriver driver;
-  private AADriver myDriver;
-  private static Properties properties;
-  private String portal;
-  private String environment;
+private WebDriverFactory contextSteps;
+private WebDriver driver;
+private AADriver myDriver;
+private static Properties properties;
+private String portal;
+private String environment;
 
 
 
-  public feature1StepDef(WebDriverFactory contentSteps){
-    this.driver = contentSteps.getDriver();
-    myDriver = new AADriver(driver);
-    this.properties = Settings.getProperties();
-  }
+public feature1StepDef(WebDriverFactory contentSteps){
+this.driver = contentSteps.getDriver();
+myDriver = new AADriver(driver);
+this.properties = Settings.getProperties();
+}
 
 
-  @After
-  public void tearDown(Scenario scenario) throws InterruptedException{
-    if (scenario.isFailed()) {
-      Set<String> windows = driver.getWindowHandles();
-      driver.switchTo().window(Iterables.getLast(windows));
-      try {
-        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        scenario.write("Scenario Failed!");
-        scenario.embed(screenshot, "image/png");
-      } catch (Exception e) {
+@After
+public void tearDown(Scenario scenario) throws InterruptedException{
+if (scenario.isFailed()) {
+Set<String> windows = driver.getWindowHandles();
+driver.switchTo().window(Iterables.getLast(windows));
+try {
+byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+scenario.write("Scenario Failed!");
+scenario.embed(screenshot, "image/png");
+} catch (Exception e) {
 // TODO: handle exception
-        e.printStackTrace();
+e.printStackTrace();
 
-      }
-    }
+}
+}
 //Slow down the script
-    Thread.sleep(10000);
+Thread.sleep(10000);
 
-  }
+}
 
 
-  @Given("^user navigates to \"([^\"]*)\"$")
-  public void method1(String param11) throws Throwable {
+@Given("^user navigates to \"([^\"]*)\"$")
+public void method1(String param11) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //navigation2
-    driver.get(param11);
+driver.get(param11);
 //<CODE>
-  }
+}
 
-  @When("^user sees \"([^\"]*)\" element$")
-  public void method2(String param21) throws Throwable {
+ @When("^user sees \"([^\"]*)\" element$")
+public void method2(String param21) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //exists1
-    feature1Page feature1Page = new feature1Page(driver);
-    feature1Page.elementExists(param21);
+feature1Page feature1Page = new feature1Page(driver);
+feature1Page.elementExists(param21);
 //<CODE>
-  }
+}
 
-  @Then("^user clicks \"([^\"]*)\" link$")
-  public void method3(String param31) throws Throwable {
+ @Then("^user clicks \"([^\"]*)\" link$")
+public void method3(String param31) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //link1
-    feature1Page feature1Page = new feature1Page(driver);
-    feature1Page.clickLink(param31);
+feature1Page feature1Page = new feature1Page(driver);
+feature1Page.clickLink(param31);
 //<CODE>
-  }
+}
 
-  //<METHOD>
+ //<METHOD>
 
 
 }
