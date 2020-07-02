4547094bd657beec30b5fc30afb66e4cce33abc7
Brutalation <brutalation@gmail.com>
2020-07-02 19:44:33 -0400

    Commtting File: PageObjectFile

---
+++ src/main/java/pageobject/feature1Page.java
@@ -0,0 +1,75 @@
+package pageobject;
+
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import framework.AADriver;
+
+import java.lang.reflect.Field;
+import org.openqa.selenium.WebElement;
+import static org.junit.Assert.assertEquals;
+
+//<IMPORTS>
+
+public class feature1Page {
+
+public static WebDriver driver;
+AADriver myDriver;
+public static Properties properties;
+
+
+/*Xpaths*/
+public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
+public static By searchBox = By.xpath("//input[@title='Search']");
+public static By link60 = By.xpath("//a[.='Forgot account?']");
+
+public static By link70 = By.xpath("//a[.='Forgot account?']");
+
+//<XPATHS>
+
+/*Page Constructor*/
+public feature1Page(){
+
+}
+
+
+public feature1Page(WebDriver driver){
+this.driver= driver;
+myDriver = new AADriver(this.driver);
+}
+
+ public boolean elementExists(String objectName) throws Exception {
+    //Element exists
+    Field field = null;
+    By by= null;
+    boolean status;
+    try {
+      field = this.getClass().getField(objectName);
+      by = (By) field.get(this);
+      status = myDriver.verifyObjectPresent(by);
+    }catch (Exception e){
+      throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
+    }
+    return status;
+  }
+
+public WebElement clickLink(String objectName) throws Exception {
+        //Click Link
+       Field field = null;
+        By by= null;
+        WebElement element=null;
+        try {
+            field = this.getClass().getField(objectName);
+            by = (By) field.get(this);
+            element = driver.findElement(by);
+            element.click();
+        }catch (Exception e){
+            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
+        }
+        return element;
+    }
+
+//<METHOD>
+}
