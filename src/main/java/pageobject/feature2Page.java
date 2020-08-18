b48aae9bbc974290f18f3b67ccd99f87e1031fea
Brutalation <brutalation@gmail.com>
2020-08-18 11:37:02 -0400

    Commtting File: PageObjectFile

---
+++ src/main/java/pageobject/feature2Page.java
@@ -0,0 +1,81 @@
+package pageobject;
+
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import framework.AADriver;
+
+import java.lang.reflect.Field;
+import org.openqa.selenium.WebElement;
+import static org.junit.Assert.assertEquals;
+
+//<IMPORTS>
+
+public class feature2Page {
+
+public static WebDriver driver;
+AADriver myDriver;
+public static Properties properties;
+
+
+/*Xpaths*/
+public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
+public static By searchBox = By.xpath("//input[@title='Search']");
+public static By text60 = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
+
+public static By text71 = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
+
+//<XPATHS>
+
+/*Page Constructor*/
+public feature2Page(){
+
+}
+
+
+public feature2Page(WebDriver driver){
+this.driver= driver;
+myDriver = new AADriver(this.driver);
+}
+
+ public boolean elementExists(String objectName) throws Exception {
+    //Element exists
+    Field field = null;
+    By by= null;
+    boolean status;
+    try {
+      field = this.getClass().getField(objectName);
+      by = (By) field.get(this);
+      status = myDriver.verifyObjectPresent(by);
+    }catch (Exception e){
+      throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
+    }
+    return status;
+  }
+
+public void writeText(String text, String objectName) throws Exception {
+        //Write Text
+        String actualText = null;
+        Field field = null;
+        By by= null;
+        WebElement element=null;
+        try {
+            field = this.getClass().getField(objectName);
+            by = (By) field.get(this);
+            element = driver.findElement(by);
+            element.clear();
+            element.sendKeys(text);
+            actualText = driver.findElement(by).getAttribute("value");
+            element=null;
+        }catch (Exception e){
+            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
+        }
+       //Assert write text
+        assertEquals(text, actualText);
+    }
+
+
+//<METHOD>
+}
