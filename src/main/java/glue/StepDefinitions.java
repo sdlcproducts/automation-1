2fcea205ca2e7bd5e5983d6cdf889104c463b00d
Brutalation <brutalation@gmail.com>
2020-08-22 19:20:07 -0400

    Commtting File: StepDefinitionFile

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -73,6 +73,16 @@ public void method3(String param31, String param32, String param33) throws Throw
 // Write code here that turns the phrase above into concrete actions
 //text2
 myDriver.typeOnPage(param31, param32, param33+"Page");
+
+//<CODE>
+}
+
+ @Then("^user clicks on \"([^\"]*)\" button$")
+public void method4(String param41) throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//button1
+feature1Page feature1Page = new feature1Page(driver);
+feature1Page.clickButton(param41);
 //<CODE>
 }
 
