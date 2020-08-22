7a2de72edb0eb87c9416459d4ebd68856024aebd
Brutalation <brutalation@gmail.com>
2020-08-22 18:30:44 -0400

    Commtting File: StepDefinitionFile

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -65,10 +65,36 @@ Thread.sleep(10000);
 }
 
 
-@Then("^user types \"([^\"]*)\" into \"([^\"]*)\" textbox on "feature1"$")
+@Given("^user navigates to \"([^\"]*)\"$")
+public void method1(String param11) throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//navigation2
+driver.get(param11);
+//<CODE>
+}
+
+ @When("^user sees \"([^\"]*)\" element$")
+public void method2(String param21) throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//exists1
+feature1Page feature1Page = new feature1Page(driver);
+feature1Page.elementExists(param21);
+//<CODE>
+}
+
+ @Then("^user types \"([^\"]*)\" into \"([^\"]*)\" textbox on "feature1" page$")
 public void method3(String param31, String param32) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 
+//<CODE>
+}
+
+ @Then("^user clicks on \"([^\"]*)\" button$")
+public void method4(String param41) throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//button1
+feature1Page feature1Page = new feature1Page(driver);
+feature1Page.clickButton(param41);
 //<CODE>
 }
 
