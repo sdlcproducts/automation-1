28efa4f992af4a2d5c67deb56f2af5ac12ec59f5
Brutalation <brutalation@gmail.com>
2020-08-23 09:52:19 -0400

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature4Page.java
+++ src/main/java/pageobject/feature4Page.java
@@ -7,8 +7,9 @@ import org.openqa.selenium.WebDriver;
 
 import framework.AADriver;
 
-import org.openqa.selenium.WebElement;
 import java.lang.reflect.Field;
+import org.openqa.selenium.WebElement;
+import static org.junit.Assert.assertEquals;
 //<IMPORTS>
 
 public class feature4Page {
