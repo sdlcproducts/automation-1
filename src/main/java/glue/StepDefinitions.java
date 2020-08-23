c149f93b361f68d5c3027efa09ea8b8e23c1ad4e
Brutalation <brutalation@gmail.com>
2020-08-23 07:49:04 -0400

    Commtting File: StepDefinitionFile

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -70,12 +70,14 @@ public void method1(String param11) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //navigation2
 driver.get(param11);
+
 //<CODE>
 }
 
  @When("^user sees \"([^\"]*)\" element on \"([^\"]*)\" page$")
 public void method2(String param21, String param22) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
+
 
 //<CODE>
 }
@@ -85,6 +87,7 @@ public void method3(String param31, String param32, String param33) throws Throw
 // Write code here that turns the phrase above into concrete actions
 //text2
 myDriver.typeOnPage(param31, param32, param33);
+
 //<CODE>
 }
 
@@ -93,6 +96,7 @@ public void method4(String param41, String param42) throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //button1
 myDriver.clickOnPage(param41, param42);
+
 //<CODE>
 }
 
