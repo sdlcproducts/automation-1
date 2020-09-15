538b3cbeb0b28fb7ad90c9bc4296b75158499e45
Mehraj <brualation@gmail.com>
2020-09-15 10:59:43 -0400

    Fixing automation

--- src/main/java/pageobject/feature1Page.java
+++ src/main/java/pageobject/feature1Page.java
@@ -7,34 +7,37 @@ import org.openqa.selenium.WebDriver;
 
 import framework.AADriver;
 
+import java.lang.reflect.Field;
+import org.openqa.selenium.WebElement;
+import static org.junit.Assert.assertEquals;
 //<IMPORTS>
 
 public class feature1Page {
 
-public static WebDriver driver;
-AADriver myDriver;
-public static Properties properties;
+    public static WebDriver driver;
+    AADriver myDriver;
+    public static Properties properties;
 
 
-/*Xpaths*/
-public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
-public static By searchBox = By.xpath("//input[@title='Search']");
-public static By defaultText = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
+    /*Xpaths*/
+    public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
+    public static By searchBox = By.xpath("//input[@title='Search']");
+    public static By SearchBarText = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
 
-public static By defaultButton = By.xpath("(//input[contains(@class,'gNO89b')])[2]");
+    public static By defaultButton = By.xpath("(//input[@class='gNO89b'])[1]");
 
 //<XPATHS>
 
-/*Page Constructor*/
-public feature1Page(){
+    /*Page Constructor*/
+    public feature1Page(){
 
-}
+    }
 
 
-public feature1Page(WebDriver driver){
-this.driver= driver;
-myDriver = new AADriver(this.driver);
-}
+    public feature1Page(WebDriver driver){
+        this.driver= driver;
+        myDriver = new AADriver(this.driver);
+    }
 
 //<METHOD>
 }
