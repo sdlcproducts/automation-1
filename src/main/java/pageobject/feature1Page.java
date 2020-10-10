09ee4b45fc19ea1f256fcb9eb912a9ed42518e37
Brutalation <brutalation@gmail.com>
2020-10-10 09:49:59 -0400

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature1Page.java
+++ src/main/java/pageobject/feature1Page.java
@@ -14,30 +14,30 @@ import static org.junit.Assert.assertEquals;
 
 public class feature1Page {
 
-    public static WebDriver driver;
-    AADriver myDriver;
-    public static Properties properties;
+public static WebDriver driver;
+AADriver myDriver;
+public static Properties properties;
 
 
-    /*Xpaths*/
-    public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
-    public static By searchBox = By.xpath("//input[@title='Search']");
-    public static By SearchBarText = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
+/*Xpaths*/
+public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
+public static By searchBox = By.xpath("//input[@title='Search']");
+public static By SearchBarText = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
 
-    public static By defaultButton = By.xpath("(//input[@class='gNO89b'])[1]");
+public static By defaultButton = By.xpath("(//input[@class='gNO89b'])[1]");
 
 //<XPATHS>
 
-    /*Page Constructor*/
-    public feature1Page(){
+/*Page Constructor*/
+public feature1Page(){
 
-    }
+}
 
 
-    public feature1Page(WebDriver driver){
-        this.driver= driver;
-        myDriver = new AADriver(this.driver);
-    }
+public feature1Page(WebDriver driver){
+this.driver= driver;
+myDriver = new AADriver(this.driver);
+}
 
 //<METHOD>
 }
