eef49f47362aa7a5ce5e5c4475687a31405dbc5e
Brutalation <brutalation@gmail.com>
2021-06-14 14:33:37 -0400

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature1Page.java
+++ src/main/java/pageobject/feature1Page.java
@@ -21,7 +21,7 @@ public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]"
 public static By searchBox = By.xpath("//input[@title='Search']");
 public static By defaultText = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
 
-public static By defaultButton = By.xpath("(//input[contains(@id,'gbqfbb')])[2]");
+public static By defaultButton = By.xpath("(//input[contains(@class,'gNO89b')])[2]");
 
 //<XPATHS>
 
