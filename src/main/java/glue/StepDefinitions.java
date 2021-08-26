aa1d06277ba09a6b498c4f5f369746c04fa5b781
Brutalation <brutalation@gmail.com>
2021-08-26 12:47:22 -0400

    Commtting File: StepDefinitionFile

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -76,6 +76,18 @@ public void method2(String param21, String param22) throws Throwable {
 myDriver.verifyObjectPresentOnPage(param21, param22);//<CODE>
 }
 
+ @When("^user types \"([^\"]*)\" into \"([^\"]*)\" textbox on \"([^\"]*)\" page$")
+public void method3(String param31, String param32, String param33) throws Throwable {
+// Write code here that turns the phrase above into concrete actions//text2
+myDriver.typeOnPage(param31, param32, param33);//<CODE>
+}
+
+ @When("^user clicks on \"([^\"]*)\" button on \"([^\"]*)\" page$")
+public void method4(String param41, String param42) throws Throwable {
+// Write code here that turns the phrase above into concrete actions//button1
+myDriver.clickOnPage(param41, param42);//<CODE>
+}
+
  //<METHOD>
 
 
