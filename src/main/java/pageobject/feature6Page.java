69db6511fc8c4055cbbae19ebd6b058490da2490
Brutalation <brutalation@gmail.com>
2021-10-14 12:15:47 -0400

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature6Page.java
+++ src/main/java/pageobject/feature6Page.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.WebDriver;
 
 import framework.AADriver;
 
+import java.lang.reflect.Field;
 import org.openqa.selenium.WebElement;
+import static org.junit.Assert.assertEquals;
+
 //<IMPORTS>
 
 public class feature6Page {
@@ -20,7 +23,11 @@ public static Properties properties;
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
 public static By searchBox = By.xpath("//input[@title='Search']");
-public static By defaultText = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy')])[1]");
+public static By text61 = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy')])[1]");
+
+public static By text63 = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy _9npi')])[1]");
+
+public static By button70 = By.xpath("//button");
 
 //<XPATHS>
 
@@ -34,6 +41,22 @@ public feature6Page(WebDriver driver){
 this.driver= driver;
 myDriver = new AADriver(this.driver);
 }
+
+public WebElement clickButton(String objectName) throws Exception {
+        //Write Text
+       Field field = null;
+        By by= null;
+        WebElement element=null;
+        try {
+            field = this.getClass().getField(objectName);
+            by = (By) field.get(this);
+            element = driver.findElement(by);
+            element.click();
+        }catch (Exception e){
+            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
+        }
+        return element;
+    }
 
 //<METHOD>
 }
