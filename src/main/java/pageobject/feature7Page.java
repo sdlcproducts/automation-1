f7fc69d30ac69e74cd224fa4b7cbf1f12e0c232b
Brutalation <brutalation@gmail.com>
2021-10-14 12:12:45 -0400

    Commtting File: PageObjectFile

---
+++ src/main/java/pageobject/feature7Page.java
@@ -0,0 +1,56 @@
+package pageobject;
+
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import framework.AADriver;
+
+import java.lang.reflect.Field;
+import org.openqa.selenium.WebElement;
+import static org.junit.Assert.assertEquals;
+
+//<IMPORTS>
+
+public class feature7Page {
+
+public static WebDriver driver;
+AADriver myDriver;
+public static Properties properties;
+
+
+/*Xpaths*/
+public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
+public static By searchBox = By.xpath("//input[@title='Search']");
+//<XPATHS>
+
+/*Page Constructor*/
+public feature7Page(){
+
+}
+
+
+public feature7Page(WebDriver driver){
+this.driver= driver;
+myDriver = new AADriver(this.driver);
+}
+
+public WebElement clickButton(String objectName) throws Exception {
+        //Write Text
+       Field field = null;
+        By by= null;
+        WebElement element=null;
+        try {
+            field = this.getClass().getField(objectName);
+            by = (By) field.get(this);
+            element = driver.findElement(by);
+            element.click();
+        }catch (Exception e){
+            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
+        }
+        return element;
+    }
+
+//<METHOD>
+}
