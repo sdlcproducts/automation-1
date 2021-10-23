77e1282f6581e492d62b165c6c58aa5b2542d770
Brutalation <brutalation@gmail.com>
2021-10-23 11:19:29 -0400

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature6Page.java
+++ src/main/java/pageobject/feature6Page.java
@@ -25,9 +25,9 @@ public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]"
 public static By searchBox = By.xpath("//input[@title='Search']");
 public static By text61 = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy')])[1]");
 
-public static By text63 = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy _9npi')])[1]");
+public static By text71 = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy _9npi')])[1]");
 
-public static By button70 = By.xpath("//button");
+public static By button80 = By.xpath("//button");
 
 //<XPATHS>
 
@@ -41,6 +41,28 @@ public feature6Page(WebDriver driver){
 this.driver= driver;
 myDriver = new AADriver(this.driver);
 }
+
+public void writeText(String text, String objectName) throws Exception {
+        //Write Text
+        String actualText = null;
+        Field field = null;
+        By by= null;
+        WebElement element=null;
+        try {
+            field = this.getClass().getField(objectName);
+            by = (By) field.get(this);
+            element = driver.findElement(by);
+            element.clear();
+            element.sendKeys(text);
+            actualText = driver.findElement(by).getAttribute("value");
+            element=null;
+        }catch (Exception e){
+            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
+        }
+       //Assert write text
+        assertEquals(text, actualText);
+    }
+
 
 public WebElement clickButton(String objectName) throws Exception {
         //Write Text
