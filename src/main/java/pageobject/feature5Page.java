10eb99f0d8ee1bb705aed32ca296e697705c47de
Brutalation <brutalation@gmail.com>
2021-12-14 06:52:39 -0500

    Commtting File: PageObjectFile

---
+++ src/main/java/pageobject/feature5Page.java
@@ -0,0 +1,44 @@
+package pageobject;
+
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import framework.AADriver;
+
+import org.openqa.selenium.WebElement;
+import java.lang.reflect.Field;
+//<IMPORTS>
+
+public class feature5Page {
+
+public static WebDriver driver;
+AADriver myDriver;
+public static Properties properties;
+
+
+/*Xpaths*/
+public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
+public static By searchBox = By.xpath("//input[@title='Search']");
+public static By defaultText = By.xpath("(//input[contains(@class,'input__input')])[1]");
+
+public static By defaultText = By.xpath("(//input[contains(@class,'input__input')])[2]");
+
+public static By SignInButtonButton = By.xpath("(//button[contains(.,'Sign in')])[1]");
+
+//<XPATHS>
+
+/*Page Constructor*/
+public feature5Page(){
+
+}
+
+
+public feature5Page(WebDriver driver){
+this.driver= driver;
+myDriver = new AADriver(this.driver);
+}
+
+//<METHOD>
+}
