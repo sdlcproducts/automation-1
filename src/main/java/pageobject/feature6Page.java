db7e09e0dec91affd9462e16fac371f025b9ca9a
Brutalation <brutalation@gmail.com>
2021-12-15 14:44:41 -0500

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature6Page.java
+++ src/main/java/pageobject/feature6Page.java
@@ -7,10 +7,6 @@ import org.openqa.selenium.WebDriver;
 
 import framework.AADriver;
 
-import java.lang.reflect.Field;
-import org.openqa.selenium.WebElement;
-import static org.junit.Assert.assertEquals;
-
 //<IMPORTS>
 
 public class feature6Page {
@@ -23,11 +19,11 @@ public static Properties properties;
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
 public static By searchBox = By.xpath("//input[@title='Search']");
-public static By text61 = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy')])[1]");
+public static By defaultText = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy')])[1]");
 
-public static By text63 = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy _9npi')])[1]");
+public static By defaultText = By.xpath("(//input[contains(@class,'inputtext _55r1 _6luy _9npi')])[1]");
 
-public static By button70 = By.xpath("//button");
+public static By LoginButton = By.xpath("//button");
 
 //<XPATHS>
 
@@ -41,44 +37,6 @@ public feature6Page(WebDriver driver){
 this.driver= driver;
 myDriver = new AADriver(this.driver);
 }
-
-public void writeText(String text, String objectName) throws Exception {
-        //Write Text
-        String actualText = null;
-        Field field = null;
-        By by= null;
-        WebElement element=null;
-        try {
-            field = this.getClass().getField(objectName);
-            by = (By) field.get(this);
-            element = driver.findElement(by);
-            element.clear();
-            element.sendKeys(text);
-            actualText = driver.findElement(by).getAttribute("value");
-            element=null;
-        }catch (Exception e){
-            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
-        }
-       //Assert write text
-        assertEquals(text, actualText);
-    }
-
-
-public WebElement clickButton(String objectName) throws Exception {
-        //Write Text
-       Field field = null;
-        By by= null;
-        WebElement element=null;
-        try {
-            field = this.getClass().getField(objectName);
-            by = (By) field.get(this);
-            element = driver.findElement(by);
-            element.click();
-        }catch (Exception e){
-            throw new Exception(objectName+" not found in "+ this.getClass().getName()+".class\n"  + e);
-        }
-        return element;
-    }
 
 //<METHOD>
 }
