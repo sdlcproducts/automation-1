99c48016ee28e8d8d2457c43b71c9c9b55f8ece6
sdlcco <68667210+sdlcco@users.noreply.github.com>
2021-12-20 14:45:25 -0500

    Update StepDefinitions.java

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -12,6 +12,8 @@ import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.But;
 import framework.AADriver;
 import framework.WebDriverFactory;
 
@@ -66,78 +68,6 @@ Thread.sleep(10000);
 public void method1(String param11) throws Throwable {
 // Write code here that turns the phrase above into concrete actions//navigation2
 driver.get(param11);//<CODE>
-}
-
-
-
- @When("^user sees \"([^\"]*)\" element on \"([^\"]*)\" page$")
-public void method2(String param21, String param22) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//exists1
-myDriver.verifyObjectPresentOnPage(param21, param22);//<CODE>
-}
-
- @When("^user types \"([^\"]*)\" into \"([^\"]*)\" textbox on \"([^\"]*)\" page$")
-public void method3(String param31, String param32, String param33) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//text2
-myDriver.typeOnPage(param31, param32, param33);//<CODE>
-}
-
- @When("^user clicks on \"([^\"]*)\" button on \"([^\"]*)\" page$")
-public void method4(String param41, String param42) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//button1
-myDriver.clickOnPage(param41, param42);//<CODE>
-}
-
-
- @When("^user enters \"([^\"]*)\" in \"([^\"]*)\" textbox in \"([^\"]*)\" page$")
-public void method6(String param51, String param52, String param53) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//text2
-myDriver.typeOnPage(param51, param52, param53);//<CODE>
-}
-
- @When("^user enters \"([^\"]*)\" in \"([^\"]*)\" textbox$")
-public void method7(String param51, String param52) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//text1
-myDriver.typeOnPage(RandomStringUtils.randomAlphanumeric(20), param51, param52);//<CODE>
-}
-
-
-
- @When("^user enters \"([^\"]*)\" in \"([^\"]*)\" textbox on \"([^\"]*)\" page$")
-public void method7(String param21, String param22, String param23) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//text2
-myDriver.typeOnPage(param21, param22, param23);//<CODE>
-}
-
- @Then("^user clicks \"([^\"]*)\" button on \"([^\"]*)\" page$")
-public void method8(String param41, String param42) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//button1
-myDriver.clickOnPage(param41, param42);//<CODE>
-}
-
-
- @Then("^user enters \"([^\"]*)\" in \"([^\"]*)\" textbox on \"([^\"]*)\" page$")
-public void method13(String param31, String param32, String param33) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//text2
-myDriver.typeOnPage(param31, param32, param33);//<CODE>
-}
-
- @Then("^user clicks on \"([^\"]*)\" button on \"([^\"]*)\" page$")
-public void method14(String param51, String param52) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//button1
-myDriver.clickOnPage(param51, param52);//<CODE>
-}
-
- @Then("^user enters \"([^\"]*)\" on \"([^\"]*)\" textbox on \"([^\"]*)\" page$")
-public void method15(String param61, String param62, String param63) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//text2
-myDriver.typeOnPage(param61, param62, param63);//<CODE>
-}
-
- @When("^user enter \"([^\"]*)\" in \"([^\"]*)\" textbox$")
-public void method16(String param21, String param22) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//text1
-myDriver.typeOnPage(RandomStringUtils.randomAlphanumeric(20), param21, param22);//<CODE>
 }
 
  @When("^user enters \"([^\"]*)\" into \"([^\"]*)\" textbox in \"([^\"]*)\" page$")
