c65cfb8b1155deb4f694b78ded0301247a0a9590
Brutalation <brutalation@gmail.com>
2022-02-10 16:40:42 -0500

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature11Page.java
+++ src/main/java/pageobject/feature11Page.java
@@ -21,15 +21,17 @@ public static Properties properties;
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
 public static By searchBox = By.xpath("//input[@title='Search']");
+public static By ChainButton = By.xpath("//i[@class='network-display__icon app-header__network-down-arrow']");
+
+public static By AvaxButton = By.xpath("(//span[.='Avalanche Mainnet C-Chain'])[last()]");
+
 public static By ClaimRewardsButton = By.xpath("//span[.='Claim Rewards']/..");
 
 public static By ConfirmButton = By.xpath("//button[.='Confirm']");
 
-public static By ChainButton = By.xpath("//i[@class='network-display__icon app-header__network-down-arrow']");
+public static By ConnectWalletButton = By.xpath("//button[.='Connect Wallet']");
 
-public static By EthMainetButton = By.xpath("(//span[.='Ethereum Mainnet'])[last()]");
-
-public static By BinanceSmartChainButton = By.xpath("(//span[.='Binance Smart Chain'])[last()]");
+public static By HydrateButton = By.xpath("//button[contains(.,'HYDRATE')]");
 
 //<XPATHS>
 
